6f229b16fa25e07d78a3b0402fe7b48c0d7f1c0f
Reiduarig <giraudier82@gmail.com>
2025-03-31 12:59:04 +0200

    Definir primeros tipos

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -27,6 +27,12 @@ export interface SharedData {
     quote: { message: string; author: string };
     auth: Auth;
     ziggy: Config & { location: string };
+    flash: {
+        success?: string;
+        error?: string;
+        warning?: string;
+        info?: string;
+    };
     [key: string]: unknown;
 }
 
@@ -40,3 +46,39 @@ export interface User {
     updated_at: string;
     [key: string]: unknown; // This allows for additional properties...
 }
+
+export interface Task {
+    id: number;
+    user_id: number;
+    title: string;
+    description: string | null;
+    due_date: string | null;
+    completed: boolean;
+    created_at: string;
+    updated_at: string;
+    user?: User;
+}
+
+export interface PaginationPageLink {
+    url: string | null;
+    label: string;
+    active: boolean;
+}
+
+export interface Pagination<T> {
+    current_page: number;
+    data: T[];      // Array de dados genéricos, puede ser cualquier tipo
+    first_page_url: string;
+    from: number;
+    last_page: number;
+    last_page_url: string;
+    links: PaginationPageLink[];
+    next_page_url: string | null;
+    path: string;
+    per_page: number;
+    prev_page_url: string | null;
+    to: number | null;
+    total: number;
+}
+
+export type PaginatedTask = Pagination<Task>;
